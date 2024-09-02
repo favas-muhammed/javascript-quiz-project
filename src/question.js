a302d86934465ddfc283aba4c00f16c7b8855848
Favas Muhammed <favassakkeer13@gmail.com>
2024-09-02 17:53:00 +0200

    first commit - favas and Alexandro

--- src/question.js
+++ src/question.js
@@ -1,7 +1,35 @@
 class Question {
     // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+    
+    constructor (text, choices, answer, difficulty){
 
-    // 2. shuffleChoices()
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
+
+ shuffleChoices(){
+
+    for (let i = this.choices.length - 1; i > 0; i--) {
+         const j = Math.floor(Math.random() * (i + 1));
+         [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]];
+
+    }
+    }
 }
+
+
+    const question1 = new Question( " What is question 1?" , ["answer1", "answer 2", "answer 3", "answer 4"], "answer 3" , 3);
+question1.shuffleChoices();
+    const question2 = new Question( " What is question 2?" , ["answer1", "answer 2", "answer 3", "answer 4"], "answer 2" , 2);
+    question2.shuffleChoices();
+    const question3 = new Question( " What is question 3?" , ["answer1", "answer 2", "answer 3", "answer 4"], "answer 2" , 1);
+    question3.shuffleChoices();
+    const question4 = new Question( " What is question 4?" , ["answer1", "answer 2", "answer 3", "answer 4"], "answer 1" , 3);  
+    question4.shuffleChoices();
+
+
+
+ 
+
--- src/quiz.js
+++ src/quiz.js
@@ -2,14 +2,45 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining){
 
-    // 2. getQuestion()
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
     
-    // 3. moveToNextQuestion()
+    }
 
-    // 4. shuffleQuestions()
 
-    // 5. checkAnswer(answer)
 
-    // 6. hasEnded()
+        getQuestion() {
+            return this.questions[this.currentQuestionIndex];
+        }
+          
+
+    moveToNextQuestion() {
+        this.currentQuestionIndex +=1;
+    }
+
+
+     shuffleQuestions()
+{
+    for (let i = this.questions.length - 1; i > 0; i--) {
+    const b = Math.floor(Math.random() * (i + 1));
+    [this.questions[i], this.questions[b]] = [this.questions[b], this.questions[i]];
+}
+}
+
+checkAnswer(answer){
+if(this.getQuestion().answer == answer){
+    this.correctAnswers +=1;
+}
+        
+}
+
+
+    hasEnded(){
+return this.currentQuestionIndex >= this.questions.length; 
+}
 }
